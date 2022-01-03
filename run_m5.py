42374387717918a696d0b6efa757507dbd6ad97d
mach1ne <luis_roque@live.com>
2022-01-03 16:50:46 +0000

    added run_m5 for all algos

---
+++ run_m5.py
@@ -0,0 +1,33 @@
+import gpforecaster as gpf
+import htsmodels as hts
+import tsaugmentation as tsag
+import matplotlib.pyplot as plt
+import os
+
+dataset_name = 'm5'
+dataset = tsag.preprocessing.PreprocessDatasets(dataset_name)
+groups = dataset.apply_preprocess()
+
+algorithm = 'gpf'
+gpf_model = gpf.model.GPF(f'{dataset_name}_{algorithm}', groups)
+model, like = gpf_model.train()
+mean, lower, upper = gpf_model.predict(model, like)
+res = gpf_model.metrics(mean)
+gpf_model.store_metrics(res)
+
+algorithm = 'mint'
+mint = hts.models.MinT(dataset=f'{dataset_name}_{algorithm}',
+                                   groups=groups,
+                                   aggregate_key = '(Category / Department) * (State / Store / Item)')
+forecasts = mint.train()
+results = mint.results(forecasts)
+res = mint.metrics(results)
+mint.store_metrics(res)
+
+algorithm = 'deepar'
+deepar = hts.models.DeepAR(dataset=f'{dataset_name}_{algorithm}', groups=groups)
+model = deepar.train()
+forecasts = deepar.predict(model)
+results = deepar.results(forecasts)
+res = deepar.metrics(results)
+deepar.store_metrics(res)
