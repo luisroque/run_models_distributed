1b47dba4449e732131d337f603c623edc2ab9e8e
mach1ne <luis_roque@live.com>
2022-01-09 12:01:14 +0000

    ansible playbook splitted

--- run_models_distributed.sh
+++ run_models_distributed.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# copy playbooks to ansible folder
+cp setup_playbook.yml /etc/ansible/setup_playbook.yml
+cp run_models_playbook.yml /etc/ansible/run_models_playbook.yml
+
+# run ansible setup file
+ansible-playbook /etc/ansible/setup_playbook.yml
+
+# copy data files to servers
+# this task needs to be preceded by the ansible setup
+# because that is the moment that the directories are
+# pulled from github fresh
 scp -r data/ mach2ne:/home/mach2ne/run_models_distributed/
 scp -r data/ coral:/home/lroque/run_models_distributed/
 scp -r data/ marineye:/home/lroque/run_models_distributed/
 scp -r data/ nitro:/home/lroque/run_models_distributed/
 
-ansible-playbook /etc/ansible/sample.yml
+# run ansible run file
+ansible-playbook /etc/ansible/run_models_playbook.yml
