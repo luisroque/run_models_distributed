51c158a0d872278637509b2134a813879df2b14f
mach1ne <luis_roque@live.com>
2022-01-09 09:32:12 +0000

    updating version

--- run_models_distributed.sh
+++ run_models_distributed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scp -r data/ mach2ne:/home/lroque/run_models_distributed/
+scp -r data/ mach2ne:/home/mach2ne/run_models_distributed/
 scp -r data/ coral:/home/lroque/run_models_distributed/
 scp -r data/ marineye:/home/lroque/run_models_distributed/
 scp -r data/ nitro:/home/lroque/run_models_distributed/
