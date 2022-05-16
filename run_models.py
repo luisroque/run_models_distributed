ae74d347e2800a119c8a73673dadb951a701436a
mach1ne <luis_roque@live.com>
2022-05-16 22:20:31 +0100

    Adding possibility to run using standard GPs

--- run_models.py
+++ run_models.py
@@ -3,6 +3,7 @@ import gpforecaster as gpf
 import htsmodels as hts
 import tsaugmentation as tsag
 import argparse
+
 warnings.filterwarnings("ignore")
 
 
@@ -18,52 +19,83 @@ def create_groups_from_data(dataset_name):
 def create_transformations(dataset_name):
     # Create transformed datasets
     data = tsag.transformations.CreateTransformedVersions(dataset_name)
-    
+
     # Parameters for the tourism dataset
-    data.parameters = {"jitter": 1.5,
-                       "scaling": 0.3,
-                       "magnitude_warp": 0.3,
-                       "time_warp": 0.005}
+    data.parameters = {
+        "jitter": 1.5,
+        "scaling": 0.3,
+        "magnitude_warp": 0.3,
+        "time_warp": 0.005,
+    }
 
     data.create_new_version_single_transf()
 
 
-def run_original_algorithm(dataset_name, algorithms, transformations, groups, aggregate_key):
+def run_original_algorithm(
+    dataset_name, algorithms, transformations, groups, aggregate_key
+):
     for algorithm in algorithms:
         for k in transformations:
             # run algorithms for the original version of the dataset
-            if algorithm == 'deepar':
-                run_deepar(dataset=f'{dataset_name}_{algorithm}_{k}_orig_s0', groups=groups)
-            elif algorithm == 'mint':
-                run_mint(dataset=f'{dataset_name}_{algorithm}_{k}_orig_s0',
-                         groups=groups,
-                         aggregate_key=aggregate_key)
-            elif algorithm == 'gpf':
-                run_gpf(dataset=f'{dataset_name}_{algorithm}_{k}_orig_s0', groups=groups)
+            if algorithm == "deepar":
+                run_deepar(
+                    dataset=f"{dataset_name}_{algorithm}_{k}_orig_s0", groups=groups
+                )
+            elif algorithm == "mint":
+                run_mint(
+                    dataset=f"{dataset_name}_{algorithm}_{k}_orig_s0",
+                    groups=groups,
+                    aggregate_key=aggregate_key,
+                )
+            elif algorithm == "gpf":
+                run_gpf(
+                    dataset=f"{dataset_name}_{algorithm}_{k}_orig_s0", groups=groups
+                )
+            elif algorithm == "standard_gp_lin":
+                run_standard_gp(
+                    dataset=f"{dataset_name}_{algorithm}_{k}_orig_s0", groups=groups
+                )
+            elif algorithm == "standard_gp_pie":
+                run_standard_gp(
+                    dataset=f"{dataset_name}_{algorithm}_{k}_orig_s0", groups=groups
+                )
 
 
-def run_algorithm(dataset_name, algorithms, transformations, groups, vis, aggregate_key):
+def run_algorithm(
+    dataset_name, algorithms, transformations, groups, vis, aggregate_key
+):
     for algorithm in algorithms:
         for k in transformations:
             # run algorithms for the transformed versions of the dataset
-            vis._read_files(f'single_transf_{k}')
+            vis._read_files(f"single_transf_{k}")
             for i in range(6):
                 for j in range(10):
-                    groups['train']['data'] = vis.y_new[i, j]
-                    if algorithm == 'deepar':
-                        run_deepar(dataset=f'{dataset_name}_{algorithm}_{k}_v{i}_s{j}', groups=groups)
-                    elif algorithm == 'mint':
-                        run_mint(dataset=f'{dataset_name}_{algorithm}_{k}_v{i}_s{j}', 
-                                 groups=groups,
-                                 aggregate_key=aggregate_key)
-                    elif algorithm == 'gpf':
-                        run_gpf(dataset=f'{dataset_name}_{algorithm}_{k}_v{i}_s{j}', groups=groups)
+                    groups["train"]["data"] = vis.y_new[i, j]
+                    if algorithm == "deepar":
+                        run_deepar(
+                            dataset=f"{dataset_name}_{algorithm}_{k}_v{i}_s{j}",
+                            groups=groups,
+                        )
+                    elif algorithm == "mint":
+                        run_mint(
+                            dataset=f"{dataset_name}_{algorithm}_{k}_v{i}_s{j}",
+                            groups=groups,
+                            aggregate_key=aggregate_key,
+                        )
+                    elif algorithm == "gpf":
+                        run_gpf(
+                            dataset=f"{dataset_name}_{algorithm}_{k}_v{i}_s{j}",
+                            groups=groups,
+                        )
 
 
 def run_deepar(dataset, groups):
-    deepar = hts.models.DeepAR(dataset=dataset, groups=groups, 
-                               store_prediction_samples=False,
-                               store_prediction_points=False)
+    deepar = hts.models.DeepAR(
+        dataset=dataset,
+        groups=groups,
+        store_prediction_samples=False,
+        store_prediction_points=False,
+    )
     model = deepar.train()
     forecasts = deepar.predict(model)
     samples = deepar.results(forecasts)
@@ -72,11 +104,13 @@ def run_deepar(dataset, groups):
 
 
 def run_mint(dataset, groups, aggregate_key):
-    mint = hts.models.MinT(dataset=dataset,
-                           groups=groups,
-                           aggregate_key=aggregate_key, 
-                           store_prediction_samples=False,
-                           store_prediction_points=False)
+    mint = hts.models.MinT(
+        dataset=dataset,
+        groups=groups,
+        aggregate_key=aggregate_key,
+        store_prediction_samples=False,
+        store_prediction_points=False,
+    )
     forecasts = mint.train()
     df_results = mint.results(forecasts)
     res = mint.metrics(df_results)
@@ -84,10 +118,20 @@ def run_mint(dataset, groups, aggregate_key):
 
 
 def run_gpf(dataset, groups):
-    gpf_model = gpf.model.GPF(dataset, groups, 
-                              store_prediction_samples=False,
-                              store_prediction_points=False)
+    gpf_model = gpf.model.GPF(
+        dataset, groups, store_prediction_samples=False, store_prediction_points=False
+    )
     model, like = gpf_model.train()
+    samples = gpf_model.predict(model, like)
+    res = gpf_model.metrics(samples)
+    gpf_model.store_metrics(res)
+
+
+def run_standard_gp(dataset, groups, mean_func='linear'):
+    gpf_model = hts.models.SGP(
+        dataset, groups, store_prediction_samples=False, store_prediction_points=False
+    )
+    model, like = gpf_model.train(mean_function=mean_func)
     samples = gpf_model.predict(model, like)
     res = gpf_model.metrics(samples)
     gpf_model.store_metrics(res)
@@ -95,35 +139,55 @@ def run_gpf(dataset, groups):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--algorithm', '-a', 
-                        nargs='+',
-                        help="select an algorithm",
-                        type=str,
-                        default=['gpf', 'mint', 'deepar'])
-    parser.add_argument('--transformation', '-t', 
-                        nargs='+',
-                        help="select a transformation",
-                        type=str,
-                        default=['jitter', 'scaling', 'magnitude_warp', 'time_warp'])
-    parser.add_argument('--dataset', '-d', 
-                        nargs='+',
-                        help="select a dataset",
-                        type=str,
-                        default=['tourism'])
-    parser.add_argument('--execution', '-e', 
-                        nargs='+',
-                        help="select type of execution",
-                        type=str,
-                        default=['original'])
+    parser.add_argument(
+        "--algorithm",
+        "-a",
+        nargs="+",
+        help="select an algorithm",
+        type=str,
+        default=["gpf", "mint", "deepar"],
+    )
+    parser.add_argument(
+        "--transformation",
+        "-t",
+        nargs="+",
+        help="select a transformation",
+        type=str,
+        default=["jitter", "scaling", "magnitude_warp", "time_warp"],
+    )
+    parser.add_argument(
+        "--dataset",
+        "-d",
+        nargs="+",
+        help="select a dataset",
+        type=str,
+        default=["tourism"],
+    )
+    parser.add_argument(
+        "--execution",
+        "-e",
+        nargs="+",
+        help="select type of execution",
+        type=str,
+        default=["original"],
+    )
     algo_transf = {}
     for k, v in parser.parse_args()._get_kwargs():
         algo_transf[k] = v
 
-    assert all(x in ['gpf', 'mint', 'deepar'] for x in algo_transf['algorithm']), 'The algorithm is not implemented'        
-    assert all(x in ['jitter', 'scaling', 'magnitude_warp', 'time_warp'] for x in algo_transf['transformation']), \
-        'Transformation not implemented'
-    assert all(x in ['tourism', 'prison', 'm5', 'police'] for x in algo_transf['dataset']), 'Dataset not implemented'
-    assert all(x in ['original', 'transformed'] for x in algo_transf['execution']), 'Execution not implemented'
+    assert all(
+        x in ["gpf", "mint", "deepar", "standard_gp_lin", "standard_gp_pie"] for x in algo_transf["algorithm"]
+    ), "The algorithm is not implemented"
+    assert all(
+        x in ["jitter", "scaling", "magnitude_warp", "time_warp"]
+        for x in algo_transf["transformation"]
+    ), "Transformation not implemented"
+    assert all(
+        x in ["tourism", "prison", "m5", "police"] for x in algo_transf["dataset"]
+    ), "Dataset not implemented"
+    assert all(
+        x in ["original", "transformed"] for x in algo_transf["execution"]
+    ), "Execution not implemented"
 
     return algo_transf
 
@@ -131,29 +195,33 @@ def parse_args():
 if __name__ == "__main__":
     algo_transf = parse_args()
 
-    if algo_transf['dataset'][0] == 'tourism':
-        aggregate_key = 'State * Zone * Region * Purpose'
-    elif algo_transf['dataset'][0] == 'm5':
-        aggregate_key = 'Department * Category * Store * State * Item'
-    elif algo_transf['dataset'][0] == 'police':
-        aggregate_key = 'Crime * Beat * Street * Zip'
+    if algo_transf["dataset"][0] == "tourism":
+        aggregate_key = "State * Zone * Region * Purpose"
+    elif algo_transf["dataset"][0] == "m5":
+        aggregate_key = "Department * Category * Store * State * Item"
+    elif algo_transf["dataset"][0] == "police":
+        aggregate_key = "Crime * Beat * Street * Zip"
     else:
         # Dataset is prison
-        aggregate_key = 'State * Gender * Legal'
+        aggregate_key = "State * Gender * Legal"
 
-    groups, vis = create_groups_from_data(algo_transf['dataset'][0])
-    #create_transformations(algo_transf['dataset'][0])
+    groups, vis = create_groups_from_data(algo_transf["dataset"][0])
+    # create_transformations(algo_transf['dataset'][0])
 
-    if algo_transf['execution'][0] == 'original':
-        run_original_algorithm(algo_transf['dataset'][0],
-                               algo_transf['algorithm'],
-                               algo_transf['transformation'],
-                               groups,
-                               aggregate_key)
-    elif algo_transf['execution'][0] == 'transformed':
-        run_algorithm(algo_transf['dataset'][0],
-                      algo_transf['algorithm'],
-                      algo_transf['transformation'],
-                      groups,
-                      vis,
-                      aggregate_key)
+    if algo_transf["execution"][0] == "original":
+        run_original_algorithm(
+            algo_transf["dataset"][0],
+            algo_transf["algorithm"],
+            algo_transf["transformation"],
+            groups,
+            aggregate_key,
+        )
+    elif algo_transf["execution"][0] == "transformed":
+        run_algorithm(
+            algo_transf["dataset"][0],
+            algo_transf["algorithm"],
+            algo_transf["transformation"],
+            groups,
+            vis,
+            aggregate_key,
+        )
